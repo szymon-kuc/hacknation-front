7ec67dad8dfb4f56f0c6aa0babd176acf925eb79
Bartek Papis <bartekpapis03@gmail.com>
2025-12-06 16:12:20 +0100

    list page and component

--- src/app/found-list/page.tsx
+++ src/app/found-list/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+import dataExample from "./data.json";
+import List from "@/components/List/List";
+import { IEventItem } from "@/types/types";
+
 export default function FoundListPage() {
+    const {foundItems} : {foundItems: IEventItem[]} = dataExample
+
   return (
-    <div>
-      <h1>Found List Page</h1>
-    </div>
+    <main className="main-found-list">
+      <h1 className="main-found-list__title">Lista rzeczy znalezionych</h1>
+      {(foundItems && foundItems.length > 0) && <List foundItems={foundItems} />}
+    </main>
   );
 }
