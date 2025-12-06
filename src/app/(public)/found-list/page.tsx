d2bef663293e2600e7c60d695d3b97313c84c1d2
Bartek Papis <bartekpapis03@gmail.com>
2025-12-06 17:51:47 +0100

    list page and component basic styles and logic

--- src/app/(public)/found-list/page.tsx
+++ src/app/(public)/found-list/page.tsx
@@ -2,14 +2,19 @@
 import dataExample from "./data.json";
 import List from "@/components/List/List";
 import { IEventItem } from "@/types/types";
+import "@/styles/lista-znalezionych/style.css";
 
 export default function FoundListPage() {
     const {foundItems} : {foundItems: IEventItem[]} = dataExample
 
   return (
     <main className="main-found-list">
-      <h1 className="main-found-list__title">Lista rzeczy znalezionych</h1>
-      {(foundItems && foundItems.length > 0) && <List foundItems={foundItems} />}
+      <div className="container">
+        <div className="main-found-list">
+          <h1 className="main-found-list__title">Lista rzeczy znalezionych</h1>
+          {(foundItems && foundItems.length > 0) && <List foundItems={foundItems} />}
+        </div>
+      </div>
     </main>
   );
 }
--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -4,15 +4,25 @@ import "@/components/List/style/list.css";
 export default function List({ foundItems }: { foundItems: IEventItem[] }) {
     return(
         <div className="found-list">
+            <div className="found-list__item found-list__item--heading">
+                    <div className="found-list__item__cell found-list__item__cell--id">Nr.</div>
+                    <div className="found-list__item__cell found-list__item__cell--name">Nazwa</div>
+                    <div className="found-list__item__cell found-list__item__cell--description">Opis</div>
+                    <div className="found-list__item__cell found-list__item__cell--type">Typ</div>
+                    <div className="found-list__item__cell found-list__item__cell--dateFound">Data znalezienia</div>
+                    <div className="found-list__item__cell found-list__item__cell--datePublish">Data publikacji</div>
+                    <div className="found-list__item__cell found-list__item__cell--locationFound">Miejsce znalezienia</div>
+                    <div className="found-list__item__cell found-list__item__cell--voivodeship">Województwo</div>
+                </div>
             {foundItems.map((item) => (
                 <div key={item.id} className="found-list__item">
                     <div className="found-list__item__cell found-list__item__cell--id">{item.id}</div>
                     <div className="found-list__item__cell found-list__item__cell--name">{item.name}</div>
                     <div className="found-list__item__cell found-list__item__cell--description">{item.description}</div>
-                    <div className="found-list__item__cell found-list__item__cell--dateFound">{item.dateFound}</div>
-                    <div className="found-list__item__cell found-list__item__cell--locationFound">{item.locationFound}</div>
                     <div className="found-list__item__cell found-list__item__cell--type">{item.type}</div>
+                    <div className="found-list__item__cell found-list__item__cell--dateFound">{item.dateFound}</div>
                     <div className="found-list__item__cell found-list__item__cell--datePublish">{item.datePublish}</div>
+                    <div className="found-list__item__cell found-list__item__cell--locationFound">{item.locationFound}</div>
                     <div className="found-list__item__cell found-list__item__cell--voivodeship">{item.voivodeship}</div>
                 </div>
             ))}
